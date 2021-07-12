7763a4a88860de050aa3aad13411b83f89157bff
steffenandersensahl <sandersensahl@gmail.com>
2021-07-12 16:45:20 -0500

    Adding javascript functionality to the simple calculator

--- force-app/main/default/lwc/simpleCalculator1/simpleCalculator1.js
+++ force-app/main/default/lwc/simpleCalculator1/simpleCalculator1.js
@@ -1,3 +1,42 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track } from 'lwc';
 
-export default class SimpleCalculator1 extends LightningElement {}
+export default class SimpleCalculator1 extends LightningElement {
+
+    @track currentResult;
+
+    firstNumber;
+    secondNumber;
+
+    numberChangeHandler(event) {
+        const inputBoxName = event.target.name;
+        if(inputBoxName === 'firstNumber') {
+            this.firstNumber = event.target.value;
+        }else {
+            this.secondNumber = event.target.value;
+        }
+    }
+
+    addHandler() {
+        const firstN = parseInt(this.firstNumber);
+        const secondN = parseInt(this.secondNumber);
+        this.currentResult = `The Result of ${firstN} + ${secondN} is ${firstN + secondN}`;
+    }
+
+    subtractHandler() {
+        const firstN = parseInt(this.firstNumber);
+        const secondN = parseInt(this.secondNumber);
+        this.currentResult = `The Result of ${firstN} - ${secondN} is ${firstN - secondN}`;
+    }
+
+    multiplyHandler() {
+        const firstN = parseInt(this.firstNumber);
+        const secondN = parseInt(this.secondNumber);
+        this.currentResult = `The Result of ${firstN} * ${secondN} is ${firstN * secondN}`;
+    }
+
+    divideHandler() {
+        const firstN = parseInt(this.firstNumber);
+        const secondN = parseInt(this.secondNumber);
+        this.currentResult = `The Result of ${firstN} / ${secondN} is ${firstN / secondN}`;
+    }
+}
