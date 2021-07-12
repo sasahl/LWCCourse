75f6f51a6bc64368f4b935d81348fc3bcf5eb69c
steffenandersensahl <sandersensahl@gmail.com>
2021-07-11 21:58:28 -0500

    Second commit
    Signed-off-by: steffenandersensahl <sandersensahl@gmail.com>

--- force-app/main/default/lwc/secondLWC/secondLWC.js
+++ force-app/main/default/lwc/secondLWC/secondLWC.js
@@ -1,3 +1,6 @@
 import { LightningElement } from 'lwc';
 
-export default class SecondLWC extends LightningElement {}
+export default class SecondLWC extends LightningElement {
+
+    // This is a comment
+}
