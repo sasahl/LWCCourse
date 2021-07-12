a752f6e3f4c41bf8bc12659f29393cccc5771cc6
steffenandersensahl <sandersensahl@gmail.com>
2021-07-12 10:09:45 -0500

    git

--- force-app/main/default/lwc/secondLWC/secondLWC.js
+++
@@ -1,9 +0,0 @@
-import { LightningElement } from 'lwc';
-
-export default class SecondLWC extends LightningElement {
-
-    // This is a comment
-    // Second comment
-    // Third comment
-    // Fourth comment - plus more
-}
