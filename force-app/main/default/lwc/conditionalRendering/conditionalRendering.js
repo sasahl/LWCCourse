c253b8097e710cea4e5111a805d809f7562b7283
steffenandersensahl <sandersensahl@gmail.com>
2021-07-12 13:58:51 -0500

    Adding loop struture for the component

--- force-app/main/default/lwc/conditionalRendering/conditionalRendering.js
+++ force-app/main/default/lwc/conditionalRendering/conditionalRendering.js
@@ -4,6 +4,8 @@ export default class ConditionalRendering extends LightningElement {
 
     @track displayDiv = false;
 
+    @track cityList = ["Madison", "Green Bay", "Milwaukee", "Eau Claire"];
+
     showDivHandler(event) {
         this.displayDiv = event.target.checked;
     }
